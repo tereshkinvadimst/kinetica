3e1f51a6be2d91aef102d5d9f14f1e26ee1b8781
Tereshkin Vadim <tereshkinvadimst@gmail.com>
2026-03-02 14:46:01 +0300

    Added python stub

--- src/lab/core.cc
+++ src/lab/core.cc
@@ -1,2 +1,22 @@
 
-#include "lab/config.hh"
+#include <nanobind/eigen/dense.h>
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/array.h>
+
+#include "lab/Random/xoshiro256.hh"
+
+namespace nb = nanobind;
+using namespace nb::literals;
+
+NB_MODULE(kinetics_lab, m) {
+    nb::class_<mf::xoshiro256>(m, "Xoshiro256")
+
+        .def(nb::init<mf::xoshiro256::result_type>(), "seed"_a)
+
+        // вызов как функции: rng()
+        .def("__call__", &mf::xoshiro256::operator())
+
+        // аналоги стандартного интерфейса C++ RNG
+        .def_static("min", &mf::xoshiro256::min)
+        .def_static("max", &mf::xoshiro256::max);
+}
